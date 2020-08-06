8ef6fd95c34c7d47aba436ea116a15702380a526
apskaushik <apskaushik@gmail.com>
2020-08-07 01:55:02 +0530

    Update HelloMaven.java

--- src/main/java/HelloMaven/HelloMaven.java
+++ src/main/java/HelloMaven/HelloMaven.java
@@ -5,6 +5,7 @@ public class HelloMaven {
 	public static void main(String[] args) {
 		System.out.println("Hello Maven !!!!");
 		System.out.println("This is my first maven project");
+		System.out.println("After lot of effort finally I compiled Maven project !!!");
 
 	}
 
