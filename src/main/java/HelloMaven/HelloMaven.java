51748dc58fef13a113e8db3ede3e58c141511c18
apskaushik <apskaushik@gmail.com>
2020-08-07 01:37:33 +0530

    Add files via upload

---
+++ src/main/java/HelloMaven/HelloMaven.java
@@ -0,0 +1,11 @@
+package HelloMaven;
+
+public class HelloMaven {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Maven !!!!");
+		System.out.println("This is my first maven project");
+
+	}
+
+}
---
+++ src/test/java/HelloMaven/HelloMavenTest.java
@@ -0,0 +1,14 @@
+package HelloMaven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloMavenTest {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
